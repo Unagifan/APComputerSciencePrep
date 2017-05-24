0d178946b55e28dce4e9db59f2380e684a3150a9
UnagiFan <andykhuu425@gmail.com>
2017-05-23 23:11:26 -0700

    Comments and Settings Map
    I added some basic comments to the code infrastructure in order to help us better understand how the code flows and works. I also mapped the settings button to a empty layout screen which we can populate on a later date

---
+++ app/src/main/java/com/example/andykhuu/apcomputerscienceprep/Question.java
@@ -0,0 +1,24 @@
+package com.example.andykhuu.apcomputerscienceprep;
+
+import android.widget.TextView;
+
+import java.io.File;
+
+/**
+ * Created by frk_alkhuu on 5/23/2017.
+ */
+
+public class Question {
+
+    private File unit;
+    private String question;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+    private String answer5;
+
+    public Question(File file){
+
+    }
+}
