2bc57845a50fe5e7cdd9a771f2bcfa36e26ec251
Unagifan <andykhuu425@gmail.com>
2017-05-26 08:38:52 -0700

    TextFile Reader Now working
    In this session I got textFile reader to work. I have also created a list of question objects and have began testing with it's usage. It's still a little clunky but with the question layout code we should be able to test it out

--- app/src/main/java/com/example/andykhuu/apcomputerscienceprep/UnitActivity.java
+++ app/src/main/java/com/example/andykhuu/apcomputerscienceprep/UnitActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -20,35 +22,70 @@ public class UnitActivity extends AppCompatActivity {
 
     private int currentUnitId;
 
+    private List<String> unitData;
+    private List<Question> questions;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.unitlayout_main);
+        this.unitData = getUnitData();
+        questions = new ArrayList<>();
 
-        //Get the current unit passed in
-        currentUnitId = getIntent().getIntExtra("UNITID", 0);
-
-        List<String> mLines = new ArrayList<>();
-        InputStream is = null;
-        //Base off the current Unit pressed, get the unit text file related.
-        switch (currentUnitId) {
-            case 0:
-                 is = getResources().getAssets().open("testFile.txt");
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
+        for(int i = 0; i < unitData.size()-1; i+=4){
+            Question temp = new Question(unitData.get(i),unitData.get(i+1),unitData.get(i+2)
+                    ,unitData.get(i+3),unitData.get(i+4),unitData.get(i+5));
+            questions.add(temp);
         }
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        String line;
-        while ((line = reader.readLine()) != null)
-            mLines.add(line);
+    }
 
+    /**
+     * Depending on the current unit pressed retrieve the text file related to that unit and
+     * read its content to a list
+     * @return ArrayList of Strings which contains the questions
+     */
+    private List<String> getUnitData(){
+        try {
+            //Get the current unit passed in
+            currentUnitId = getIntent().getIntExtra("UNITID", 0);
+
+            unitData = new ArrayList<>();
+            InputStream is;
+            //Base off the current Unit pressed, get the unit text file related.
+            switch (currentUnitId) {
+                case 0:
+                    is = getResources().getAssets().open("testFile.txt");
+                    break;
+                case 1:
+                    is = getResources().getAssets().open("testFile.txt");
+                    break;
+                case 2:
+                    is = getResources().getAssets().open("testFile.txt");
+                    break;
+                case 3:
+                    is = getResources().getAssets().open("testFile.txt");
+                    break;
+                case 4:
+                    is = getResources().getAssets().open("testFile.txt");
+                    break;
+                case 5:
+                    is = getResources().getAssets().open("testFile.txt");
+                    break;
+                case 6:
+                    is = getResources().getAssets().open("testFile.txt");
+                    break;
+                default:
+                    is = getResources().getAssets().open("testFile.txt");
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = reader.readLine()) != null)
+                unitData.add(line);
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return unitData;
     }
 }
+
