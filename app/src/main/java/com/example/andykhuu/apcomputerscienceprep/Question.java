57d1692e4419dba787b3ded0f7e932e93deccb63
UnagiFan <andykhuu425@gmail.com>
2017-05-29 00:49:14 -0700

    Clean Up and Layout Stuff
    Most importantly I deleted a folder from the res package which was causing a problem for the code. I also am experimenting with the xml code necessary to test my object class.

--- app/src/main/java/com/example/andykhuu/apcomputerscienceprep/Question.java
+++ app/src/main/java/com/example/andykhuu/apcomputerscienceprep/Question.java
@@ -6,6 +6,11 @@ import java.io.File;
 
 /**
  * Created by frk_alkhuu on 5/23/2017.
+ * The Question class is the code for the object which is known as Question.
+ * Hypothetically from the text file this should get and contain all the necessary contents
+ * to make a single question work.
+ * 1 Question String
+ * 5 Answer Strings
  */
 
 public class Question {
--- app/src/main/java/com/example/andykhuu/apcomputerscienceprep/UnitActivity.java
+++ app/src/main/java/com/example/andykhuu/apcomputerscienceprep/UnitActivity.java
@@ -3,6 +3,8 @@ package com.example.andykhuu.apcomputerscienceprep;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -24,6 +26,7 @@ public class UnitActivity extends AppCompatActivity {
 
     private List<String> unitData;
     private List<Question> questions;
+    private String[] questionNumbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -32,18 +35,28 @@ public class UnitActivity extends AppCompatActivity {
         this.unitData = getUnitData();
         questions = new ArrayList<>();
 
-        for(int i = 0; i < unitData.size()-1; i+=4){
+        //From unitData convert the given answers into question objects and put it into a list of questions.
+        for(int i = 0; i < unitData.size()-1; i+=6){
             Question temp = new Question(unitData.get(i),unitData.get(i+1),unitData.get(i+2)
                     ,unitData.get(i+3),unitData.get(i+4),unitData.get(i+5));
             questions.add(temp);
         }
+        //
+        this.questionNumbers = new String[questions.size()];
+        for(int i = 0; i < questionNumbers.length-1;i++){
+            questionNumbers[i] = "Question " + i;
+        }
+
+        ArrayAdapter<String> num = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,questionNumbers);
+        ListView listView = (ListView) findViewById(R.id.QuestionsList);
+        listView.setAdapter(num);
 
     }
 
     /**
-     * Depending on the current unit pressed retrieve the text file related to that unit and
+     * Depending on the current unit pressed, retrieve the text file related to that unit and
      * read its content to a list
-     * @return ArrayList of Strings which contains the questions
+     * @return ArrayList of Strings which contains the questions data
      */
     private List<String> getUnitData(){
         try {
