7561b13483ac45bc0a3909c5019bb87ca5fd3e24
UnagiFan <andykhuu425@gmail.com>
2017-06-06 23:10:05 -0700

    YESS
    In this grind out learning xml session I finally learned/discovered how to put a background on textviews in both xml and java(for versatility). I implemented a file to show/demonstrate how to have a universal background be applied on textviews such as answers. In the end I was able to create a layout, using borders, that would visually show the currently selected answer.

--- app/src/main/java/com/example/andykhuu/apcomputerscienceprep/QuestionActivity.java
+++ app/src/main/java/com/example/andykhuu/apcomputerscienceprep/QuestionActivity.java
@@ -20,6 +20,13 @@ public class QuestionActivity extends AppCompatActivity {
     private int correctAnswer;
     private int selected;
 
+    private TextView question;
+    private TextView answer0;
+    private TextView answer1;
+    private TextView answer2;
+    private TextView answer3;
+    private TextView answer4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,56 +38,69 @@ public class QuestionActivity extends AppCompatActivity {
         QUESTION = getIntent().getParcelableExtra("QUESTION");
         //Determine which feedbackText contains the correct answer
         correctAnswer = QUESTION.getCorrectAnswer();
-
+        //Map questions and answers to layout and setup answer selection logic
         answerSelection();
     }
 
+    /**
+     * Fills out the layout for the question activity and sets up the answer selection logic/system
+     */
     private void answerSelection(){
-        TextView question = (TextView) findViewById(R.id.question);
+        question = (TextView) findViewById(R.id.question);
         question.setText(QUESTION.getQuestion());
 
-        TextView answer0 = (TextView) findViewById(R.id.answer0);
+        answer0 = (TextView) findViewById(R.id.answer0);
         answer0.setText(QUESTION.getAnswers()[0]);
         answer0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selected = 0;
+                nullBackground();
+                answer0.setBackgroundResource(R.drawable.selectedborder);
             }
         });
 
-        TextView answer1 = (TextView) findViewById(R.id.answer1);
+        answer1 = (TextView) findViewById(R.id.answer1);
         answer1.setText(QUESTION.getAnswers()[1]);
         answer1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selected = 1;
+                nullBackground();
+                answer1.setBackgroundResource(R.drawable.selectedborder);
             }
         });
 
-        TextView answer2 = (TextView) findViewById(R.id.answer2);
+        answer2 = (TextView) findViewById(R.id.answer2);
         answer2.setText(QUESTION.getAnswers()[2]);
         answer2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selected = 2;
+                nullBackground();
+                answer2.setBackgroundResource(R.drawable.selectedborder);
             }
         });
 
-        TextView answer3 = (TextView) findViewById(R.id.answer3);
+        answer3 = (TextView) findViewById(R.id.answer3);
         answer3.setText(QUESTION.getAnswers()[3]);
         answer3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selected = 3;
+                nullBackground();
+                answer3.setBackgroundResource(R.drawable.selectedborder);
             }
         });
 
-        TextView answer4 = (TextView) findViewById(R.id.answer4);
+        answer4 = (TextView) findViewById(R.id.answer4);
         answer4.setText(QUESTION.getAnswers()[4]);
         answer4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selected = 4;
+                nullBackground();
+                answer4.setBackgroundResource(R.drawable.selectedborder);
             }
         });
 
@@ -103,5 +123,13 @@ public class QuestionActivity extends AppCompatActivity {
             }
         });
     }
+    private void nullBackground(){
+        answer0.setBackground(null);
+        answer1.setBackground(null);
+        answer2.setBackground(null);
+        answer3.setBackground(null);
+        answer4.setBackground(null);
+    }
+
 }
 
